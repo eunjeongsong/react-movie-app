e8d598019179f5ec68d67f10a486ff85da8a8a14
eunjeongsong <hellogine12@gmail.com>
2018-11-01 02:23:05 +0900

    Updates

---
+++ precache-manifest.46ddcce876acba6bc30ea47f4e635c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdcfacafbc62971f5461",
+    "url": "/react-movie-app/static/js/runtime~main.bdcfacaf.js"
+  },
+  {
+    "revision": "77befcb785b6483d9ce9",
+    "url": "/react-movie-app/static/js/main.77befcb7.chunk.js"
+  },
+  {
+    "revision": "1fb62fa53e8c646a78bc",
+    "url": "/react-movie-app/static/js/1.1fb62fa5.chunk.js"
+  },
+  {
+    "revision": "77befcb785b6483d9ce9",
+    "url": "/react-movie-app/static/css/main.3963f6e1.chunk.css"
+  },
+  {
+    "revision": "a5f9c66c828e23198233bcfde2a3e779",
+    "url": "/react-movie-app/index.html"
+  }
+];
